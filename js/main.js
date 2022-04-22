674af2df482fdeeadda7ae2f5500c5fe3cca8750
Matheus Sanders Montandon <msmontandon@gmail.com>
2022-04-22 22:42:22 +0000

    Confirm that there are no errors in the console while using the application. #3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 var $header = document.querySelector('header');
 var $pickButton = $header.querySelector('[data-view="pick"]');
-var $pickButtonMobile = $header.querySelector('[data-view="pick mobile"]');
+var $pickButtonMobile = $header.querySelector('[data-view="pick-mobile"]');
+var $search = $header.querySelector('[data-view="search"]');
+var $searchMobile = $header.querySelector('[data-view="search-mobile"]');
 var $container = document.querySelector('.container');
 var $menu = document.querySelector('.menu');
 var $mobileRows = $header.querySelectorAll('.mobile');
@@ -13,22 +15,18 @@ var $foregroundTextDescription = $foreground.querySelector('#card-description');
 var $foregroundTextUp = $foreground.querySelector('#card-up');
 var $foregroundTextReverse = $foreground.querySelector('#card-reverse');
 
-function Spread() {
-  this.cards = [];
-}
-
-Spread.prototype.setTable = function () {
+function setTable() {
   if (data.currentTable) {
     data.currentTable.remove();
-    data.currentCards = [];
+    data.currentSpread = [];
     data.cards = [];
   }
   data.currentTable = document.createElement('div');
   data.currentTable.className = 'row card-display';
   $container.appendChild(data.currentTable);
-};
+}
 
-Spread.prototype.pickCard = function (quantity) {
+function pickCard(quantity) {
   var cardRequest = new XMLHttpRequest();
   cardRequest.open('GET', 'https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=' + quantity);
   cardRequest.responseType = 'json';
@@ -56,7 +54,7 @@ Spread.prototype.pickCard = function (quantity) {
       $cardImg.setAttribute('title', data.cards[currentCardIndex].name);
       $cardElement.appendChild($cardImg);
 
-      data.currentCards.push({
+      data.currentSpread.push({
         cardColumn: $cardColumn,
         cardWrap: $cardWrap,
         cardElement: $cardElement,
@@ -66,12 +64,12 @@ Spread.prototype.pickCard = function (quantity) {
     }
   });
   cardRequest.send();
-};
+}
 
 function pickOne() {
-  data.currentSpread = new Spread();
-  data.currentSpread.setTable();
-  data.currentSpread.pickCard(1);
+  // data.currentSpread = new Spread();
+  setTable();
+  pickCard(1);
 }
 
 function resetForeground() {
@@ -81,8 +79,59 @@ function resetForeground() {
     $foregroundTextName.closest('.description-wrapper').className += ' hidden';
     data.descriptionHidden = true;
   }
-  data.currentCards[data.selectedIndex].cardWrap.className = data.currentCards[data.selectedIndex].cardWrap.className.replace(' hidden', '');
+  data.currentSpread[data.selectedIndex].cardWrap.className = data.currentSpread[data.selectedIndex].cardWrap.className.replace(' hidden', '');
   data.selectedIndex = null;
+}
+
+function search(event) {
+  event.preventDefault();
+  if (!data.foregroundHidden) {
+    resetForeground();
+  }
+  setTable();
+  // console.log(event.target.elements.search.value);
+  var searchContent = event.target.elements.search.value;
+  event.target.elements.search.value = '';
+  // console.log(event.target.elements.search.value);
+  var searchRequest = new XMLHttpRequest();
+  searchRequest.open('GET', 'https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=' + searchContent);
+  searchRequest.responseType = 'json';
+  searchRequest.addEventListener('load', function () {
+    data.searchVolume = searchRequest.response.nhits;
+    data.nextSearchIndex = 0;
+    for (var i = 0; i < data.searchVolume; i++) {
+      data.cards.push(searchRequest.response.cards[i]);
+      var currentCardIndex = data.cards.length - 1;
+      data.cards[currentCardIndex].image = 'archive/cards/' + data.cards[currentCardIndex].name_short + '.jpg';
+      var $cardColumn = document.createElement('div');
+      $cardColumn.className = 'column-card';
+      data.currentTable.appendChild($cardColumn);
+
+      var $cardWrap = document.createElement('div');
+      $cardWrap.className = 'card-wrapper';
+      $cardWrap.setAttribute('data-card-index', currentCardIndex);
+      $cardColumn.appendChild($cardWrap);
+
+      var $cardElement = document.createElement('div');
+      $cardElement.className = 'card';
+      $cardWrap.appendChild($cardElement);
+
+      var $cardImg = document.createElement('img');
+      $cardImg.setAttribute('src', data.cards[currentCardIndex].image);
+      $cardImg.setAttribute('alt', data.cards[currentCardIndex].name);
+      $cardImg.setAttribute('title', data.cards[currentCardIndex].name);
+      $cardElement.appendChild($cardImg);
+
+      data.currentSpread.push({
+        cardColumn: $cardColumn,
+        cardWrap: $cardWrap,
+        cardElement: $cardElement,
+        cardImg: $cardColumn,
+        card: data.cards[currentCardIndex]
+      });
+    }
+  });
+  searchRequest.send();
 }
 
 $pickButton.addEventListener('click', function () {
@@ -111,7 +160,7 @@ $menu.addEventListener('click', function (event) {
   }
 });
 window.addEventListener('resize', function (event) {
-  if (event.target.innerWidth >= 415 && !data.mobileHidden) {
+  if (event.target.innerWidth >= 550 && !data.mobileHidden) {
     for (var i = 0; i < $mobileRows.length; i++) {
       $mobileRows[i].className += ' hidden';
     }
@@ -124,13 +173,13 @@ $container.addEventListener('click', function (event) {
     data.foregroundHidden = false;
     event.target.closest('.card-wrapper').className += ' hidden';
     data.selectedIndex = parseInt(event.target.closest('.card-wrapper').getAttribute('data-card-index'));
-    $foregroundCard.setAttribute('src', data.currentCards[data.selectedIndex].card.image);
-    $foregroundCard.setAttribute('alt', data.currentCards[data.selectedIndex].card.name);
-    $foregroundCard.setAttribute('title', data.currentCards[data.selectedIndex].card.name);
-    $foregroundTextName.textContent = data.currentCards[data.selectedIndex].card.name;
-    $foregroundTextDescription.textContent = data.currentCards[data.selectedIndex].card.desc;
-    $foregroundTextUp.textContent = data.currentCards[data.selectedIndex].card.meaning_up;
-    $foregroundTextReverse.textContent = data.currentCards[data.selectedIndex].card.meaning_rev;
+    $foregroundCard.setAttribute('src', data.currentSpread[data.selectedIndex].card.image);
+    $foregroundCard.setAttribute('alt', data.currentSpread[data.selectedIndex].card.name);
+    $foregroundCard.setAttribute('title', data.currentSpread[data.selectedIndex].card.name);
+    $foregroundTextName.textContent = data.currentSpread[data.selectedIndex].card.name;
+    $foregroundTextDescription.textContent = data.currentSpread[data.selectedIndex].card.desc;
+    $foregroundTextUp.textContent = data.currentSpread[data.selectedIndex].card.meaning_up;
+    $foregroundTextReverse.textContent = data.currentSpread[data.selectedIndex].card.meaning_rev;
   }
 });
 
@@ -151,3 +200,6 @@ $foreground.addEventListener('click', function (event) {
     resetForeground();
   }
 });
+
+$search.addEventListener('submit', search);
+$searchMobile.addEventListener('submit', search);
